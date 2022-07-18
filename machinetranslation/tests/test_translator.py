ed7f30734505d46b8ad3bbe1098b1e3cda5ee9b9
saifc8 <109261349+saifc8@users.noreply.github.com>
2022-07-18 20:21:45 +0300

    Create test_translator.py

---
+++ machinetranslation/tests/test_translator.py
@@ -0,0 +1,19 @@
+import unittest
+
+from translator import english_to_french, french_to_english
+
+
+class TestEnglishToFrench(unittest.TestCase):
+    def test1(self):
+        self.assertIsNotEqual(english_to_french(''), None)
+        self.assertEqual(english_to_french('Hello'), 'Bonjour')
+
+
+class TestFrenchToEnglish(unittest.TestCase):
+    def test1(self):
+        self.assertIsNotEqual(french_to_english(''), None)
+        self.assertEqual(french_to_english('Bonjour'), 'Hello')
+
+
+if __name__ == '__main__':
+    unittest.main()
